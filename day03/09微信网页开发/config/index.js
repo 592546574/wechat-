0c691405b40b101abb6a72be8d4dbb0ae39aa4da
huifang <592546574@qq.com>
2018-11-20 13:40:06 +0800

    微信的网页开发-CDK配置完成

--- day03/09微信网页开发/config/index.js
+++ day03/09微信网页开发/config/index.js
@@ -2,5 +2,5 @@ module.exports={
     appID:'wx4458ce39ec461257',
     appsecret:'3d2ac3680fbb40d3ad1191c086747710',
     token:'zhang11/16',
-    url:'http://7f24ead8.ngrok.io'
+    url:'http://ce3f5130.ngrok.io'
 }
